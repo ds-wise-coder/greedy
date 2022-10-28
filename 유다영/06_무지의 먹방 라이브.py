a55f575bd19bdc691f66caebb4f418a380a93970
allzeroyou <0fcoursewhynot@duksung.ac.kr>
2022-10-28 23:59:33 +0900

    1028

--- 유다영/06_무지의 먹방 라이브.py
+++ 유다영/06_무지의 먹방 라이브.py
@@ -1,29 +1,36 @@
-'''
-가장 적은 비용의 음식부터 먹어야 함 -> 우선순위 큐(heqp)
-'''
+# 각 음식을 모두 먹는데 필요한 시간이 담겨있는 배열 food_times
+# 네트워크 장애가 발생한 시간 K초
+# 몇 번 음식부터 다시 섭취하면 되는 지 return 하도록 solution 함수 완성
 
-import heapq
+from operator import itemgetter
 
 
 def solution(food_times, k):
-    # 모든 음식 섭취 시간 합 == k 시간보다 같거나 작다면 모두 섭취한 것 -> -1
-    if sum(food_times) <= k:
-        return -1
+    foods = []  # 음식을 먹는 데 필요한 시간, 음식의 번호를 함께 저장해서 정렬하기 위한 리스트 정의
+    n = len(food_times)  # 음식의 총 개수
+    for i in range(n):  # 음식의 개수 만큼 foods 리스트에 추가
+        foods.append((food_times[i], i + 1))  # 시간, 음식 번호(1~n)
+    foods.sort()  # 먹는 시간 순 오름차순 정렬
 
-    q = []
-    for i in range(len(food_times)):
-        heapq.heappush(q, (food_times[i], i + 1))
+    # 이전 음식 시간과의 간격을 구함
+    pretime = 0  # 이전 음식
+    for i, food in enumerate(foods):  # 인덱스& vaule 필요
+        diff = food[0] - pretime  # 그래프에서의 높이, 간격 차
+        if diff != 0:
+            spend = diff * n  # 소비한 시간 = diff * 남은 음식 개수
+            # k에서 spend를 빼주기
+            if spend <= k:
+                k -= spend
+                pretime = food[0]
+            else:
+                k %= n  # 남은 시간을 남은 음식 개수로 나머지 연산
+                # 남은 음식은 순서대로 먹어야 함.
+                # foods[i:]: i~끝까지.
+                sublist = sorted(foods[i:], key=itemgetter(1))  # 음식번호(인덱스 1)로 오름차순 정렬
+                return sublist[k][1] # 인덱스 1: 음식 번호 반환
+        n -= 1  # turn이 끝날때 마다 음식 하나를 다 먹은 것.
+    return -1 # 더 섭취할 음식이 없다면
 
-    sum_value = 0  # 그동안 먹었던 음식의 총 합
-    previous = 0  # 바로 직전에 먹은 음식을 먹는데 소요되는 시간
-    length = len(food_times)  # 남은 음식 개수
 
-    # 지금까지 이전 음식 먹는 데 사용한 총 시간 + (이제 먹을 음식 시간 - 직전에 먹은 음식 시간) * 남은 음식 개수 -> 직전에 먹은 음식 시간을 빼주는 이유는 회전판이 돌 동안 이제 먹을 음식을 일부 먹었을 것이므로!
-    while sum_value + (q[0][0] - previous) * length <= k:
-        now = heapq.heappop(q)[0]  # 이제 먹을 음식 시간
-        sum_value += (now - previous) * length  # 이제 먹을 음식 중 남은 시간 * 남은 음식 개수
-        length -= 1
-        previous = now  # 이제 먹은 음식을 이전에 먹은 음식으로 갱신
-
-    result = sorted(q, key=lambda x: x[1])  # 음식 번호순으로 큐를 재정렬
-    return result[(k - sum_value) % length][1]
+result = solution([3, 1, 2], 5)
+print(result)
